a92e3504d00ee93cba1584330d9195d6c4a86154
Peter Wang <pwang649@usc.edu>
2022-04-17 18:03:40 -0700

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,13 +8,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://pwang649.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'pwang649', // Usually your GitHub org/user name.
+  projectName: 'myWiki', // Usually your repo name.
 
   presets: [
     [
